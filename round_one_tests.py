ecf81d4bacda48d9262a9f4912e5fac4a25dc3fe
NatashaMabon <natashamabon@jmangroup.com>
2022-03-08 15:24:01 +0000

    Add dropdown to test app

--- round_one_tests.py
+++ round_one_tests.py
@@ -6,6 +6,7 @@
 import dash
 from dash import html
 from dash import dcc
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import plotly.express as px
 
@@ -14,25 +15,37 @@ import plotly.express as px
 app = dash.Dash()  # initialising dash app
 df = px.data.stocks()  # reading stock price dataset
 
-def stock_prices():
-    # Function for creating line chart showing Google stock prices over time
-    fig = go.Figure([go.Scatter(x=df['date'], y=df['GOOG'], \
-                                line=dict(color='firebrick', width=4), name='Google')
+app.layout = html.Div(id='parent',
+                      children=[
+                          html.H1(id='H1', children='Styling using html components', style={'textAlign': 'center',
+                                                                                            'marginTop': 40,
+                                                                                            'marginBottom': 40}),
+
+                          dcc.Dropdown(id='dropdown',
+                                       options=[
+                                           {'label': 'Google', 'value': 'GOOG'},
+                                           {'label': 'Apple', 'value': 'AAPL'},
+                                           {'label': 'Amazon', 'value': 'AMZN'},
+                                       ],
+                                       value='GOOG'),
+                          dcc.Graph(id='bar_plot')
+                      ])
+
+
+@app.callback(Output(component_id='bar_plot', component_property='figure'),
+              [Input(component_id='dropdown', component_property='value')])
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    fig = go.Figure([go.Scatter(x=df['date'], y=df['{}'.format(dropdown_value)], \
+                                line=dict(color='firebrick', width=4))
                      ])
-    fig.update_layout(title='Prices over time',
+
+    fig.update_layout(title='Stock prices over time',
                       xaxis_title='Dates',
                       yaxis_title='Prices'
                       )
     return fig
 
-
-app.layout = html.Div(id='parent', children=[
-    html.H1(id='H1', children='Styling using html components', style={'textAlign': 'center', \
-                                                                      'marginTop': 40, 'marginBottom': 40}),
-
-    dcc.Graph(id='line_plot', figure=stock_prices())
-]
-                      )
 
 if __name__ == '__main__':
     app.run_server()
